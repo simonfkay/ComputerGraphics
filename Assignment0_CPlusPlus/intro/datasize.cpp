e4a95a105e8a55075845c718fe9f0344221c0293
simonfkay <simonfkay@me.com>
2020-01-09 00:40:18 -0500

    Git ignore and Part 1

---
+++ Assignment0_CPlusPlus/intro/datasize.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Data size of int in bytes = " << sizeof(int) << std::endl;
+    std::cout << "Data size of char in bytes = " << sizeof(char) << std::endl;    
+    std::cout << "Data size of float in bytes = " << sizeof(float) << std::endl;
+    std::cout << "Data size of double in bytes = " << sizeof(double) << std::endl;
+
+    return 0;
+}
